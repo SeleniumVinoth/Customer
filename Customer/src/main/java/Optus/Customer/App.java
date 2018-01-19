cba42e9efe59f1f0b3bfbc015595e4e337f02db1
Vinoth Kannan <vinothkannan.chandran@gmail.com>
2018-01-19 06:51:32 +0530

    Code Commited

---
+++ Customer/src/main/java/Optus/Customer/App.java
@@ -0,0 +1,13 @@
+package Optus.Customer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
